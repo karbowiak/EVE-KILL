8fca1aa734cc5b52a910bac65371ac2a40c6d19d
Michael Karbowiak <karbowiak@gmail.com>
2017-06-09 00:01:54 +0200

    Fix so tests succeed

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -13,8 +13,8 @@ class IndexController extends Controller {
 	 * @return Response
 	 */
 	public function index(string $name = null): Response {
-		var_dump($this->dbAsync->executeQuery("select", "SELECT * FROM account_api_key_info_characters"));
-		var_dump($this->dbAsync->getQuery("select"));
+		//var_dump($this->dbAsync->executeQuery("select", "SELECT * FROM account_api_key_info_characters"));
+		//var_dump($this->dbAsync->getQuery("select"));
 		return $this->render("home.twig", array("name" => $name));
 	}
 }
